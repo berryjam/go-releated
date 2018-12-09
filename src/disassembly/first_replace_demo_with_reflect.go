57763115cdc888fe90a3ebd4c72fc0611d29f79a
huangjinkun <huangjinkun@didichuxing.com>
2018-12-10 02:17:30 +0800

    update

--- src/disassembly/first_replace_demo_with_reflect.go
+++ src/disassembly/first_replace_demo_with_reflect.go
@@ -40,20 +40,15 @@ func getPageWithReflect(p uintptr) []byte {
 	return (*(*[0xFFFFFF]byte)(unsafe.Pointer(p & ^uintptr(syscall.Getpagesize() - 1))))[:syscall.Getpagesize()]
 }
 
-func RawMemoryAccessWithReflect(p uintptr, l int) []byte {
-	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: p,
-		Len:  l,
-		Cap:  l,
-	}))
-	//return (*(*[0xFF]byte)(unsafe.Pointer(p)))[:]
+func RawMemoryAccessWithReflect(p uintptr) []byte {
+	return (*(*[0xFF]byte)(unsafe.Pointer(p)))[:]
 }
 
 func replaceWithReflect(from, to interface{}) {
 
 	jumpAndExecToAssemblyData := JumpAssemblyDataWithReflect(reflect.ValueOf(to).Pointer())
 	funcLocation := reflect.ValueOf(from).Pointer()
-	window := RawMemoryAccessWithReflect(funcLocation, len(jumpAndExecToAssemblyData))
+	window := RawMemoryAccessWithReflect(funcLocation)
 
 	page := getPageWithReflect(funcLocation)
 	syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC)
