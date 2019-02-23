e1b602630f05a46b08edc69c2494fa1e008609e8
huangjinkun <huangjinkun@didichuxing.com>
2019-02-23 17:48:29 +0800

    update

--- count_file.sh
+++ count_file.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 function count_files() {
+	cur_dir=$1
 	count=0
-	files=`ls $1`
-	for element in $files 
+	files=`ls $cur_dir`
+	for element in $files
 	do
-		if [ -f $element ];then
+		if [ -f $cur_dir/$element ];then
 			count=`expr $count + 1`
-		elif [ -d $element ];then
-			subCount=`count_files ./$element`
+		elif [ -d $cur_dir/$element ];then
+			subCount=`count_files $cur_dir/$element`
 			count=`expr $count + $subCount`
 		fi
 	done
 	echo $count
 }
 
-
-count_files ./
+count_files $1
