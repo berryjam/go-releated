0a0b9eb06d56d9905fa70795ae266889c6e4b7b2
huangjinkun <huangjinkun@didichuxing.com>
2019-03-23 16:13:44 +0800

    update

--- src/rate_limiter/counter-rate-limiter.go
+++ src/rate_limiter/counter-rate-limiter.go
@@ -12,7 +12,8 @@ var counterLuaScript = `
 	redis.pcall("zadd", KEYS[1], ARGV[1], ARGV[1]); 
 	redis.pcall("zremrangebyscore", KEYS[1], 0, ARGV[2]); 
 	local count = redis.pcall("zcard", KEYS[1]); 
-	redis.pcall("expire", KEYS[1], ARGV[3]); return count`
+	redis.pcall("expire", KEYS[1], ARGV[3]); 
+	return count`
 
 var evalSha string
 
