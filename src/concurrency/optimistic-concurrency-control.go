4ff5ca15ecd6f6a385ddf0965a4b674b2b135494
haoran <jinkun.hjk@antfin.com>
2020-08-24 11:10:57 +0800

    implement optimistic lock by CAS

---
+++ src/concurrency/optimistic-concurrency-control.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	counter int32 = 0
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	threadNum := 1000
+	wg.Add(threadNum)
+
+	for i := 0; i < threadNum; i++ {
+		go incr(i, wg)
+	}
+
+	wg.Wait()
+	fmt.Printf("counter:%+v\n", counter)
+}
+
+func incr(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	spinNum := 0
+	for {
+		old := counter
+		ok := atomic.CompareAndSwapInt32(&counter, old, old+1)
+		if ok {
+			break
+		} else {
+			spinNum++
+		}
+	}
+	//counter++
+	fmt.Printf("goroutine%+v: spinNum:%+v\n", i, spinNum)
+}
